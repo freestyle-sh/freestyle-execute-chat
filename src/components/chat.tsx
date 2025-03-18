67a401fd611e865746b9175d1e66b1a9e0620a49
Will Hopkins <willothyh@gmail.com>
2025-03-17 21:23:38 -0700

    chat submit button

--- src/components/chat.tsx
+++ src/components/chat.tsx
@@ -1,9 +1,33 @@
 "use client";
 
 import { useChat } from "@ai-sdk/react";
+import {
+  PromptInput,
+  PromptInputAction,
+  PromptInputActions,
+  PromptInputTextarea,
+} from "@/components/ui/prompt-input";
+import { Button } from "@/components/ui/button";
+import {
+  ArrowUp,
+  CommandIcon,
+  CornerDownLeft,
+  CornerDownLeftIcon,
+  Loader,
+  Loader2,
+  MonitorStopIcon,
+  Square,
+  StopCircleIcon,
+} from "lucide-react";
+import { useEffect, useState } from "react";
+import { ChatRequestOptions } from "ai";
+import { ChangeEvent } from "react";
+import { cn } from "@/lib/utils";
 
 export function ChatUI() {
-  const { messages, input, handleInputChange, handleSubmit } = useChat({});
+  const { messages, input, handleInputChange, handleSubmit, status } = useChat(
+    {},
+  );
 
   return (
     <div className="flex flex-col h-full justify-between max-w-3xl mx-auto p-6 w-full">
@@ -21,24 +45,13 @@ export function ChatUI() {
           handleSubmit={handleSubmit}
           input={input}
           handleValueChange={handleInputChange}
+          isLoading={status === "streaming"}
           // onChange={handleInputChange}
         />
       </div>
     </div>
   );
 }
-
-import {
-  PromptInput,
-  PromptInputAction,
-  PromptInputActions,
-  PromptInputTextarea,
-} from "@/components/ui/prompt-input";
-import { Button } from "@/components/ui/button";
-import { ArrowUp } from "lucide-react";
-// import { useState } from "react";
-import { ChatRequestOptions } from "ai";
-import { ChangeEvent } from "react";
 
 export function PromptInputBasic(props: {
   handleSubmit: (
@@ -48,13 +61,11 @@ export function PromptInputBasic(props: {
     chatRequestOptions?: ChatRequestOptions,
   ) => void;
   input: string;
+  isLoading: boolean;
   handleValueChange: (
     e: ChangeEvent<HTMLInputElement> | ChangeEvent<HTMLTextAreaElement>,
   ) => void;
 }) {
-  // const [input, setInput] = useState("");
-  // // const [isLoading, setIsLoading] = useState(false);
-
   // const handleValueChange = (value: string) => {
   //   setInput(value);
   // };
@@ -67,8 +78,7 @@ export function PromptInputBasic(props: {
           target: { value },
         } as ChangeEvent<HTMLTextAreaElement>)
       }
-      // isLoading={isLoading}
-      onSubmit={props.handleSubmit}
+      isLoading={props.isLoading}
       className="promptbox w-full max-w-(--breakpoint-md)"
     >
       <PromptInputTextarea
@@ -82,15 +92,21 @@ export function PromptInputBasic(props: {
         >
           <Button
             variant="default"
-            size="icon"
-            className="h-8 w-8 rounded-full"
+            size="default"
+            className={cn(
+              props.isLoading ? "w-8" : "w-14",
+              "h-8 px-3 rounded-full cursor-pointer transition-all duration-300 ease-out",
+            )}
             onClick={props.handleSubmit}
           >
-            {/* {isLoading ? (
-              <Square className="size-5 fill-current" />
-            ) : ( */}
-            <ArrowUp className="size-5" />
-            {/* )} */}
+            {props.isLoading ? (
+              <Square className="fill-secondary" />
+            ) : (
+              <span className="flex flex-row gap-0.5 items-center">
+                <CommandIcon className="size-4" />
+                <CornerDownLeftIcon className="size-4" />
+              </span>
+            )}
           </Button>
         </PromptInputAction>
       </PromptInputActions>
