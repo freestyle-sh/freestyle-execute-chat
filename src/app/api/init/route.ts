6419beedad768d225c7a80787d6afddb78265b1a
Benjamin Swerdlow <Swerdlowbenjamin@gmail.com>
2025-03-19 02:44:59 -0700

    add stripe

--- src/app/api/init/route.ts
+++ src/app/api/init/route.ts
@@ -4,6 +4,7 @@ import initResend from "./resend";
 import initPostgres from "./postgres";
 import initSupabase from "./supabase";
 import initVercel from "./vercel";
+import initStripe from "./stripe";
 
 export async function POST() {
   await db.delete(freestyleModulesTable);
@@ -12,6 +13,7 @@ export async function POST() {
   await initPostgres();
   await initSupabase();
   await initVercel();
+  await initStripe();
 
   return new Response("Initialized", { status: 200 });
 }
---
+++ src/app/api/init/stripe.ts
@@ -0,0 +1,31 @@
+import { db } from "@/db";
+import {
+  freestyleModulesEnvironmentVariableRequirementsTable,
+  freestyleModulesTable,
+} from "@/db/schema";
+import { siStripe } from "simple-icons";
+export default async function initStripe() {
+  const stripe = await db
+    .insert(freestyleModulesTable)
+    .values({
+      name: "stripe",
+      svg: siStripe.svg,
+      lightModeColor: siStripe.hex,
+      darkModeColor: siStripe.hex,
+      nodeModules: {
+        stripe: "8.214.0",
+      },
+      example: `Interact with your Stripe project`,
+    })
+    .returning();
+
+  await db.insert(freestyleModulesEnvironmentVariableRequirementsTable).values([
+    {
+      moduleId: stripe[0].id,
+      name: "STRIPE_KEY",
+      description: "Stripe key",
+      example: "sk_test_5165vg64hjgvjhvf324ahvjhvhgvghvhgvhgvhgv",
+    },
+  ]);
+  return stripe;
+}
