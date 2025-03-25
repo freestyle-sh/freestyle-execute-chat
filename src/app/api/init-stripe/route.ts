05347c419b2f0ca3ad8b4c7952e2ac9881d5ac1c
Benjamin Swerdlow <Swerdlowbenjamin@gmail.com>
2025-03-25 06:05:56 -0700

    setup stripe started

--- src/app/api/init-stripe/route.ts
+++ src/app/api/init-stripe/route.ts
@@ -4,20 +4,53 @@ export async function POST() {
   // if (request.headers.get("Authorization") !== "SuperSecretPassword") {
   //   return new Response("Nope.", { status: 401 });
   // }
-
-  await stripe.billing.meters.create({
+  const inputMeter = await stripe.billing.meters.create({
     event_name: "INPUT_TOKENS",
     display_name: "Input Tokens",
+
     default_aggregation: {
       formula: "sum",
     },
   });
 
-  await stripe.billing.meters.create({
+  const outputMeter = await stripe.billing.meters.create({
     event_name: "OUTPUT_TOKENS",
     display_name: "Output Tokens",
+
     default_aggregation: {
       formula: "sum",
+    },
+  });
+
+  const inputTokensProduct = await stripe.products.create({
+    name: "Input Tokens",
+    type: "service",
+  });
+
+  const outputTokensProduct = await stripe.products.create({
+    name: "Output Tokens",
+    type: "service",
+  });
+
+  await stripe.prices.create({
+    unit_amount_decimal: "0.000006",
+    currency: "usd",
+    nickname: "Input Tokens Price",
+    product: inputTokensProduct.id,
+    recurring: {
+      interval: "month",
+      meter: inputMeter.id,
+    },
+  });
+
+  await stripe.prices.create({
+    unit_amount_decimal: "0.000030",
+    currency: "usd",
+    nickname: "Output Tokens Price",
+    product: outputTokensProduct.id,
+    recurring: {
+      interval: "month",
+      meter: outputMeter.id,
     },
   });
 
